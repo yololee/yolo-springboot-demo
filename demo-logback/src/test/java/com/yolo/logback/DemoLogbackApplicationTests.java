75bf96613be56e98a66b6df5af3db9fa894d1e6e
yololee <huanglei421023@163.com>
2023-05-04 13:54:11 +0800

    feat: logback的使用

---
+++ demo-logback/src/test/java/com/yolo/logback/DemoLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolo.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
